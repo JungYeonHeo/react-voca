201490c1c1959de714b9ee006017fd01ecc400c6
golgol22 <heojungyeon.97@gmail.com>
2023-01-14 19:43:25 +0900

    add handling event 2

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,13 +1,24 @@
 export default function Hello() {
-	function showName() {
-		console.log('Mike');
+	function showAge(age) {
+		console.log(age);
+	}
+
+	function showText(e) {
+		console.log(e.target.value);
 	}
 
 	return (
 		<div>
 			<h1>Hello</h1>
-			<button onClick={showName}>Show name</button>
-			<button>Show age</button>
+			<button>Show name</button>
+			<button
+				onClick={() => {
+					showAge(30);
+				}}
+			>
+				Show age
+			</button>
+			<input type="text" onChange={showText} />
 		</div>
 	);
 }
